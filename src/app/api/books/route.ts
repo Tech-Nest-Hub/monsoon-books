5782f749588d482763e966f486cce886b5d6bca2
Ghost9841 <asubedi778@gmail.com>
2026-05-22 11:58:23 +0545

    Point 1

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -11,6 +11,7 @@ export async function GET(request: Request) {
   });
 }
 
+
 export async function POST(request: Request) {
     const data = await request.json();
     const newBook = await prisma.book.create({ data });
