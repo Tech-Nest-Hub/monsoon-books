9bc6d43fb3e29f0a82e77b50a941a2e69f7df3f8
Ghost9841 <asubedi778@gmail.com>
2026-05-31 17:01:46 +0545

    Layout for search of meta data

--- src/app/(client)/search/layout.tsx
+++ src/app/(client)/search/layout.tsx
@@ -1,4 +1,14 @@
 import Navbar from "@/components/navcomp/Navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Book Search | Monsoon Books",
+  description:
+    "Search thousands of books by title, author, genre, or keyword. Discover new books and find exactly what you're looking for on Monsoon Books.",
+  icons: {
+    icon: "/MonsoonBooksLogoBlack&White.ico",
+  },
+};
 
 export default function SearchLayout({
   children,
