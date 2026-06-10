15bfba98ff551978b2f3adc0db2e734b1e260de2
Ghost9841 <asubedi778@gmail.com>
2026-06-10 17:00:37 +0545

    Use DB URL aaba

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "@prisma/client"
 
-const adapter = new PrismaPg({ connectionString: process.env.DIRECT_URL })
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
